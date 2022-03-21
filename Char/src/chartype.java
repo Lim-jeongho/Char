17d87786243b10c1c7a188ae85cee469c4532a03
Lim-jeongho <limjh0387@gmail.com>
2022-03-21 22:35:47 +0900

    문자형 c 선언후 출력

--- Char/src/chartype.java
+++ Char/src/chartype.java
@@ -4,9 +4,11 @@ public class chartype {
 	public static void main(String[] args) {
 		char a = '\uac00';
 		char b = '³ª';
+		char c = 'C';
 		
 		System.out.println(a);
 		System.out.println(b);
+		System.out.println(c);
 
 	}
 
